9792fb6459a7652a75aa133f5a2acba53d796692
Muhammad Firman Pebrizal <firman.pebrizal@larisin.id>
2020-08-20 22:36:37 +0700

    define list item as aconstant function that return jsx and added to render property in list

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -3,33 +3,36 @@ import { Card, List } from 'antd';
 import './PokemonList.css';
 
 const { Meta } = Card;
+const gridOptions = {
+  gutter: [16,16],
+  xs: 1,
+  sm: 1,
+  md: 2,
+  lg: 4,
+  xl: 4,
+  xxl: 4,
+};
+
+const listItem = (pokemon) => (
+  <List.Item>
+    <Card
+      className="card-wrapper"
+      hoverable
+      cover={<img alt="example" className="card-image" src={pokemon.image_url} height="180"/>}
+    >
+      <Meta className="cart-meta" title={pokemon.name} />
+    </Card>
+  </List.Item>
+);
 
 const PokemonList = (props) => {
-    return (
-      <List
-        grid={{
-          gutter: [16,16],
-          xs: 1,
-          sm: 1,
-          md: 2,
-          lg: 4,
-          xl: 4,
-          xxl: 4,
-        }}
-        dataSource={props.pokemons}
-        renderItem={pokemon => (
-          <List.Item>
-            <Card
-              className="card-wrapper"
-              hoverable
-              cover={<img alt="example" className="card-image" src={pokemon.image_url} height="180"/>}
-            >
-              <Meta className="cart-meta" title={pokemon.name} />
-            </Card>
-          </List.Item>
-        )}
-      />
-    );
-  };
+  return (
+    <List
+      grid={gridOptions}
+      dataSource={props.pokemons}
+      renderItem={listItem}
+    />
+  );
+};
   
   export default PokemonList;
